931d03b9f447177b973775bffacaabc0f7fedde3
Developer <dev@volunteer-helper.com>
2026-05-20 09:59:39 -0400

    feat(university): expose GET /universities/ranking-board

--- apps/server/src/modules/university/university.module.ts
+++ apps/server/src/modules/university/university.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UniversityController } from './university.controller';
 import { UniversityService } from './university.service';
+import { RankingBoardService } from './ranking-board.service';
 import { AdmissionModule } from '../admission/admission.module';
 
 @Module({
   imports: [AdmissionModule],
   controllers: [UniversityController],
-  providers: [UniversityService],
+  providers: [UniversityService, RankingBoardService],
   exports: [UniversityService],
 })
 export class UniversityModule {}
