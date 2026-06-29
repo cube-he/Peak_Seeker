745cc33de02063bf236e1c446e3ef6522c988bb9
Developer <dev@volunteer-helper.com>
2026-06-30 00:38:58 +0800

    feat(plan-import): preview/commit 端点装配(controller + module 注册)

--- apps/server/src/modules/plan-import/plan-import.module.ts
+++ apps/server/src/modules/plan-import/plan-import.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PlanModule } from '../plan/plan.module';
+import { VolunteerFormParserService } from './volunteer-form-parser.service';
 import { VolunteerFormResolverService } from './volunteer-form-resolver.service';
 import { StudentBatchMatcherService } from './student-batch-matcher.service';
 import { VolunteerFormImportService } from './volunteer-form-import.service';
+import { VolunteerFormImportController } from './volunteer-form-import.controller';
 
 @Module({
   imports: [PlanModule],
-  providers: [VolunteerFormResolverService, StudentBatchMatcherService, VolunteerFormImportService],
-  exports: [VolunteerFormResolverService, StudentBatchMatcherService, VolunteerFormImportService],
+  controllers: [VolunteerFormImportController],
+  providers: [VolunteerFormParserService, VolunteerFormResolverService, StudentBatchMatcherService, VolunteerFormImportService],
+  exports: [VolunteerFormParserService, VolunteerFormResolverService, StudentBatchMatcherService, VolunteerFormImportService],
 })
 export class PlanImportModule {}
